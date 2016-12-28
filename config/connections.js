a623deb1e1439bbab138ed06300b1768c3dcba58
Phu Phan <hphu90@gmail.com>
2016-12-28 08:20:04 +0700

    fix bug

--- config/connections.js
+++ config/connections.js
@@ -44,7 +44,7 @@ module.exports.connections = {
     adapter: 'sails-mysql',
     host: 'localhost',
     user: 'root', //optional
-    password: 'CnTbpc67sOpa9', //optional
+    password: '', //optional
     database: 'letsshare' //optional
   },
 
